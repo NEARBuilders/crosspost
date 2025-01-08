a7dd7c6d6714e3fa3a5db606691dbc75480cbb46
Elliot Braem <elliot@ejlbraem.com>
2025-01-07 23:23:44 -0500

    favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -64,7 +64,7 @@ export default function App({ Component, pageProps }) {
 
         {/* Favicon */}
         <link rel="icon" href="/black.svg" />
-        <link rel="apple-touch-icon" href="/near-logo.svg" />
+        <link rel="apple-touch-icon" href="/black.svg" />
       </Head>
       <NearContext.Provider value={{ wallet, signedAccountId }}>
         <Component {...pageProps} />
