c5e00446ac8f91d9af0f4c947d29032a2df76ccc
Elliot Braem <elliot@ejlbraem.com>
2025-01-10 01:38:19 -0500

    disable twitter

--- src/components/connect-to-twitter.js
+++ src/components/connect-to-twitter.js
@@ -21,7 +21,7 @@ export function ConnectToTwitterButton() {
   return (
     <button
       onClick={handleClick}
-      disabled={isConnecting}
+      disabled={true}
       className="flex items-center gap-2 px-4 py-2 border-2 border-gray-800 hover:bg-gray-100 shadow-[2px_2px_0_rgba(0,0,0,1)] disabled:opacity-50 disabled:cursor-not-allowed"
     >
       <Twitter size={18} />
