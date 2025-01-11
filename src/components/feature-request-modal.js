dc227e4cf0b7c2685f6b68cb73fff1ebd123f25e
Elliot Braem <elliot@ejlbraem.com>
2025-01-11 16:48:46 -0500

    fix ordering

--- src/components/feature-request-modal.js
+++ src/components/feature-request-modal.js
@@ -21,7 +21,7 @@ import {
 import { Textarea } from "./ui/textarea";
 import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
 
-const PREPEND_TEXT = "@open_crosspost !submit";
+const PREPEND_TEXT = "!submit @open_crosspost";
 
 export function RequestFeatureButton({ post }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
